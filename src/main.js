96fb70be92e12b7ae2f79e6d54440cd93ff93705
laiM <laiM@example.com>
2021-08-07 21:07:56 +0800

    学生管理系统

--- src/main.js
+++ src/main.js
@@ -2,8 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios"
+import elementUi from "element-ui"
+import "element-ui/lib/theme-chalk/index.css"
+import "@/assets/css/global.css"
+/* 注意echarts  5的版本会报错 用4.1.0的版本 */
+import * as echarts from 'echarts'
+/* chart-gl 安装1.1.1 */
+import "echarts-gl"
+
+Vue.prototype.$echarts=echarts
+
+Vue.prototype.$http = axios
+Vue.use(elementUi)
 
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import index from "@/views/index"
+import shouye from "@/views/indexChild/shouye"
+import experience from "@/views/indexChild/experience"
+import excellent from "@/views/indexChild/excellent"
+import teacher from "@/views/indexChild/teacher"
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+      path:'/',
+      redirect:"/index"
+    },{
+      path:"/index",
+      component:index,
+      children:[
+        {
+          path:"/index/0",
+          component:shouye
+        },
+        {
+          path:"/index/1",
+          component:experience
+        },
+        {
+          path:"/index/2",
+          component:excellent
+        },
+        {
+          path:"/index/3",
+          component:teacher
+        },
+      ]
+    },
+    
 ]
 
 const router = new VueRouter({
